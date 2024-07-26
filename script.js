9efa55e257c7bdf864427998436f817f83d67f5a
Yusuf Arda Peker <yusufardapkr@gmail.com>
2024-07-26 14:31:48 +0300

    refactoring

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const countdownElements = document.querySelectorAll(".countdown-number");
 
-const daysElement = countdownElements[0];
-const hoursElement = countdownElements[1];
-const minutesElement = countdownElements[2];
-const secondsElement = countdownElements[3];
+const [daysElement, hoursElement, minutesElement, secondsElement] = countdownElements;
 
 const startingDays = 14;
 let time = startingDays * 24 * 60 * 60;
 
 const updateCountDown = () => {
-	let days = Math.floor(time / 60 / 60 / 24);
-	let hours = Math.floor((time / 60 / 60) % 24);
-	let minutes = Math.floor((time / 60) % 60);
-	let seconds = time % 60;
+	const days = Math.floor(time / 60 / 60 / 24);
+	const hours = Math.floor((time / 60 / 60) % 24);
+	const minutes = Math.floor((time / 60) % 60);
+	const seconds = time % 60;
 
 	days < 10 ? (daysElement.innerText = `0${days}`) : (daysElement.innerText = days);
 	hours < 10 ? (hoursElement.innerText = `0${hours}`) : (hoursElement.innerText = hours);
